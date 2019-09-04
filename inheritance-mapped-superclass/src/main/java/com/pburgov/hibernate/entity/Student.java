1f9e6834dec4636118a0165b842b20903a916834
pburgov <pburgov@gmail.com>
2019-09-04 08:52:03 +0200

    Adding Mapping Classes Inheritance Strategy MAPPEDSUPERCLASS

---
+++ inheritance-mapped-superclass/src/main/java/com/pburgov/hibernate/entity/Student.java
@@ -0,0 +1,26 @@
+package com.pburgov.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+public class Student extends User {
+
+    private String course;
+
+    public Student() {
+    }
+
+    public Student( String firstName, String lastName, String email, String course ) {
+        super(firstName, lastName, email);
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse( String course ) {
+        this.course = course;
+    }
+}
