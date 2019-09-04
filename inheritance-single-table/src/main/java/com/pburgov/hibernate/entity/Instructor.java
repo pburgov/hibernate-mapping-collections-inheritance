a30f76360e8a30147b98403341b26dd49bf97408
pburgov <pburgov@gmail.com>
2019-09-04 08:45:40 +0200

    Adding Mapping Classes Inheritance Strategy SINGLE TABLE

---
+++ inheritance-single-table/src/main/java/com/pburgov/hibernate/entity/Instructor.java
@@ -0,0 +1,27 @@
+package com.pburgov.hibernate.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue( "INSTRUCTOR" )
+public class Instructor extends User {
+
+    private Double salary;
+
+    public Instructor() {
+    }
+
+    public Instructor( String firstName, String lastName, String email, Double salary ) {
+        super(firstName, lastName, email);
+        this.salary = salary;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary( Double salary ) {
+        this.salary = salary;
+    }
+}
